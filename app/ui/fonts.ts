62bae278855458ca17ceeae541969a00ddad301c
walkerius <walkerius@yahoo.com>
2024-03-08 19:25:22 -0600

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -4,5 +4,5 @@ import { Inter, Lusitana } from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({
 	weight: ['400', '700'],
-	subsets: ['latin'],
+	subsets: ['latin'],  
 });  
