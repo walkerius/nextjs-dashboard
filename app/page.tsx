27b74b640287bdfd2de0b82758fccda70bc2d3c6
walkerius <walkerius@yahoo.com>
2024-03-08 19:44:23 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css'
 import { lusitana } from '@/app/ui/fonts';
-import Image from 'next/Image';
+import Image from 'next/image';
 
 export default function Page() {
   return (
