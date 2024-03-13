85bc60d6dc387ca0f7fe65b2e3d301dd4e7fc04a
walkerius <walkerius@yahoo.com>
2024-03-12 21:01:29 -0500

    update

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,12 +2,26 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-import { useDebouncedCallback } from 'use-debounce';
+import { useDebounce, useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
 	const searchParams = useSearchParams();
 	const pathname = usePathname();
 	const { replace } = useRouter();
+
+	const handleSearch = useDebouncedCallback((term) => {
+		console.log(`searching... ${term}`);
+		const params = new URLSearchParams(searchParams);
+
+		if (term) {
+			params.set('query', term);
+		}
+		else {//remove the imput if empty
+			params.delete('query');
+		}
+
+		replace(`${pathname}?${params.toString()}`);
+	}, 300);
 
 	return (
 		<div className="relative flex flex-1 flex-shrink-0">
@@ -22,18 +36,4 @@ export default function Search({ placeholder }: { placeholder: string }) {
 			<MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
 		</div>
 	);
-
-	function handleSearch(term: string) {
-		console.log(`searching... ${term}`);
-		const params = new URLSearchParams(searchParams);
-
-		if (term) {
-			params.set('query', term);
-		}
-		else {//remove the imput if empty
-			params.delete('query');
-		}
-
-		replace(`${pathname}?${params.toString()}`);
-	}
 }
