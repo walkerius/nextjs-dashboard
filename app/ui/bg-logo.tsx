53e4808ca1fcbf52c382f9313888be3eab9ad8c8
walkerius <walkerius@yahoo.com>
2024-04-02 20:47:50 -0500

    fixing big give image

--- app/ui/bg-logo.tsx
+++ app/ui/bg-logo.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
 import { lusitana } from '@/app/ui/fonts';
 
-export default function AcmeLogo() {
+export default function bgLogo() {
 	return (
 		<div
 			className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
 		>
 			<Image
 				className=""
-				src="/bannerImage.jpg"
+				src="/bannerimage.png"
 				alt="Next.js Logo"
-				width={400}
-				height={400}
+				width={500}
+				height={500}
 				priority
 			/>
 		</div>
