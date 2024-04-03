cd2a12415a48a6b58388ce431ba20b2dd4e93ebf
walkerius <walkerius@yahoo.com>
2024-04-02 20:20:48 -0500

    update dashboard

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -26,27 +26,27 @@ export default async function Page() {
 				<CurrentItems/>
 			</div>
 
-			<div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-				<Suspense fallback={<CardsSkeleton />}>
-					<CardWrapper />
-				</Suspense>
-				{/*<Card title="Collected" value={cardData.totalPaidInvoices} type="collected" /> */}
-				{/*<Card title="Pending" value={cardData.totalPendingInvoices} type="pending" /> */}
-				{/*<Card title="Total Invoices" value={cardData.numberOfInvoices} type="invoices" /> */}
-				{/* <Card*/}
-				{/*	title="Total Customers"*/}
-				{/*	value={cardData.numberOfCustomers}*/}
-				{/*	type="customers"*/}
-				{/*/> */}
-			</div>
-			<div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-				<Suspense fallback={<RevenueChartSkeleton />}>
-					<RevenueChart  />
-				</Suspense>
-				<Suspense fallback={<LatestInvoicesSkeleton /> }>
-					<LatestInvoices />
-				</Suspense>
-			</div>
+			{/*<div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">*/}
+			{/*	<Suspense fallback={<CardsSkeleton />}>*/}
+			{/*		<CardWrapper />*/}
+			{/*	</Suspense>*/}
+			{/*	*/}{/*<Card title="Collected" value={cardData.totalPaidInvoices} type="collected" /> */}
+			{/*	*/}{/*<Card title="Pending" value={cardData.totalPendingInvoices} type="pending" /> */}
+			{/*	*/}{/*<Card title="Total Invoices" value={cardData.numberOfInvoices} type="invoices" /> */}
+			{/*	*/}{/* <Card*/}
+			{/*	*/}{/*	title="Total Customers"*/}
+			{/*	*/}{/*	value={cardData.numberOfCustomers}*/}
+			{/*	*/}{/*	type="customers"*/}
+			{/*	*/}{/*/> */}
+			{/*</div>*/}
+			{/*<div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">*/}
+			{/*	<Suspense fallback={<RevenueChartSkeleton />}>*/}
+			{/*		<RevenueChart  />*/}
+			{/*	</Suspense>*/}
+			{/*	<Suspense fallback={<LatestInvoicesSkeleton /> }>*/}
+			{/*		<LatestInvoices />*/}
+			{/*	</Suspense>*/}
+			{/*</div>*/}
 		</main>
 	);
 }
