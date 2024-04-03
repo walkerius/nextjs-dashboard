b4a439c24e539f031b2c2af661d64863bc2bc57f
walkerius <walkerius@yahoo.com>
2024-04-02 21:12:09 -0500

    fixing naming convention

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,4 @@
-import bgLogo from '@/app/ui/bg-logo';
+import BgLogo from '@/app/ui/bg-logo';
 import LoginForm from '@/app/ui/login-form';
 
 export default function LoginPage() {
@@ -7,7 +7,7 @@ export default function LoginPage() {
 			<div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
 				<div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
 					<div className="w-32 text-white md:w-36">
-						<bgLogo />
+						<BgLogo />
 					</div>
 				</div>
 				<LoginForm />
