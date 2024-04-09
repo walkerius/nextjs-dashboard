9e0b9afda5ca77d91e3ae134a0ad62085f76aa69
walkerius <walkerius@yahoo.com>
2024-04-08 20:53:26 -0500

    fixed old page

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,4 +1,4 @@
-import Form from '@/app/ui/registration/create-form';
+import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
 
