0421b48c08a7be4e2391663051c9d841a72a265f
walkerius <walkerius@yahoo.com>
2024-05-14 20:49:45 -0500

    fix
    fix

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -13,7 +13,7 @@ import clsx from 'clsx';
 // Depending on the size of the application, this would be stored in a database.
 const links = [
 	{ name: 'Home', href: '/dashboard', icon: HomeIcon },	
-	{ name: 'Registration', href: '/dashboard/registration', icon: UserGroupIcon }
+	{ name: 'Registration', href: '/dashboard/registration', icon: UserGroupIcon },
 	{ name: 'Export', href: '/dashboard/export', icon: UserGroupIcon }
 ];
 
