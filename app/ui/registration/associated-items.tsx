1efb4838b4f44c9e8e67bf648322371e8e1353d9
walkerius <walkerius@yahoo.com>
2024-07-04 10:55:17 -0500

    Update associated-items.tsx

--- app/ui/registration/associated-items.tsx
+++ app/ui/registration/associated-items.tsx
@@ -30,7 +30,7 @@ export default async function RecipientItemTable({
 						))}
 					</div>
 					<h1 style={{ fontSize: '22px' }}>
-						{recipients.length == 0 ? 'No items associated yet.' : ''}
+						{recipients.length == 0 ? 'No items associated yet.' : 'Current Items'}
 					</h1>
 					<table className="hidden min-w-full text-gray-900 md:table">
 						<thead className="rounded-lg text-left text-sm font-normal">
