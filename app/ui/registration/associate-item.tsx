935cc6d794d2bec818ec805165290c343bc91d6d
walkerius <walkerius@yahoo.com>
2025-08-09 16:41:13 -0500

    Update associate-item.tsx
    removing the required field from notes

--- app/ui/registration/associate-item.tsx
+++ app/ui/registration/associate-item.tsx
@@ -105,7 +105,6 @@ export default function AssociateItems({
 						placeholder="Enter any special notes"
 						className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
 						aria-describedby="amount-error"
-						required
 					/>
 				</div>
 			</div>
